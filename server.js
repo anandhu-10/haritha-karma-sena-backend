b5159bdd590c244f78b24f38422f42fa36d5e57e
anandhu-10 <anandhuanil01215002@gmail.com>
2026-01-18 11:27:03 +0530

    Increase body size limit for base64 image upload

--- server.js
+++ server.js
@@ -19,8 +19,9 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
 
 /* ---------- ROUTES ---------- */
 const authRoutes = require("./routes/authroutes");
